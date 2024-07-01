1d5a48a6697539306c311d5cd3b8b26272e02703
ChuSeonju <a4707@naver.com>
2024-07-01 18:18:27 +0900

    0701

---
+++ 14/js/hw1.js
@@ -0,0 +1,13 @@
+var espresso = 10;
+var milk = 170;
+var chocoSyrup = 50;
+var whippedCream = 60;
+
+var latte = espresso + milk;
+var mocha = espresso + chocoSyrup + milk
+var whippedMocha = mocha + whippedCream
+
+console.log(espresso);
+console.log(latte);
+console.log(mocha);
+console.log(whippedMocha);
---
+++ 14/js/index.js
@@ -0,0 +1,18 @@
+// console.log("Hello JavaScript...")
+// console.log(3000 * 2);
+// console.log(4200 * 4 + 4800 * 2);
+// console.log(4200 +4800);
+// console.log(3000 + 4200 * 4 + 4800 * 2);
+// console.log(4200 * 3);
+
+var espressoPrice = 3000;
+var lattePrice = 4300;
+var mochaPrice = 4800;
+
+console.log(espressoPrice * 2);
+console.log(lattePrice * 4 + lattePrice * 2);
+console.log(lattePrice +lattePrice);
+console.log(espressoPrice + lattePrice * 4 + lattePrice * 2);
+console.log(lattePrice * 3);
+
+// 변수의 필요성. 실수로 값을 잘못 지정했을 때 변수 값만 수정해주면 되니까
